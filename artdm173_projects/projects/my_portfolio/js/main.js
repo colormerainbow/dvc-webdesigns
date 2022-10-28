8b15dd1a0a2e76fe35633736f95714b6282577b3
margaretau <aumarg.edu@gmail.com>
2022-10-27 21:29:51 -0700

    moving new header with js logic through all html pages, interim phase debugging issue with conflicts on modal css and the popup menu

--- artdm173_projects/projects/my_portfolio/js/main.js
+++ artdm173_projects/projects/my_portfolio/js/main.js
@@ -1,36 +1,18 @@
-$(function(){
-    $('.carousel-item').eq(0).addClass('active');
-    var total = $('.carousel-item').length;
-    var current = 0;
-    $('#moveRight').on('click', function(){
-      var next=current;
-      current= current+1;
-      setSlide(next, current);
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    console.log("init");
+    document.querySelector('#menu-btn').addEventListener('click', function (e) {
+        console.log("toggle #menu-btn");
+        e.preventDefault();
+        var el = document.querySelector('.main-nav');
+        el.classList.toggle('show');
     });
-    $('#moveLeft').on('click', function(){
-      var prev=current;
-      current = current- 1;
-      setSlide(prev, current);
+
+    document.querySelector('#x-burger').addEventListener('click', function (e) {
+        console.log("toggle #menu-btn");
+        e.preventDefault();
+        var elx = document.querySelector('.main-nav');
+        elx.classList.toggle('show');
     });
-    function setSlide(prev, next){
-      var slide= current;
-      if(next>total-1){
-       slide=0;
-        current=0;
-      }
-      if(next<0){
-        slide=total - 1;
-        current=total - 1;
-      }
-             $('.carousel-item').eq(prev).removeClass('active');
-             $('.carousel-item').eq(slide).addClass('active');
-        setTimeout(function(){
-  
-        },800);
-      
-  
-      
-      console.log('current '+current);
-      console.log('prev '+prev);
-    }
-  });
+}
